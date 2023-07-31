8c1ea515243e480d50c8cacb223c892e27b5d3df
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2023-07-31 16:47:20 +0300

    fix: plugin

--- includes/Actions/SendFirebaseDatabase.php
+++ includes/Actions/SendFirebaseDatabase.php
@@ -28,22 +28,6 @@ final class NF_Firebase_Actions_SendFirebaseDatabase extends NF_Abstracts_Action
    */
   protected $_priority = '10';
 
-	/**
-	 * @var array
-	 */
-  protected $endpoints = array(
-  	'realtime' => 'https://%s.firebaseio.com%s',
-	  'firestore' => 'https://firestore.googleapis.com/v1beta1/projects/%s/databases/(default)/documents%s'
-  );
-
-	/**
-	 * @var array
-	 */
-  protected $scopes = array(
-  	'realtime' => array( 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/firebase.database'),
-	  'firestore' => 'https://www.googleapis.com/auth/datastore'
-  );
-
   /**
    * Constructor
    */
@@ -53,11 +37,9 @@ final class NF_Firebase_Actions_SendFirebaseDatabase extends NF_Abstracts_Action
 
 		$settings = NF_Firebase::config( 'ActionSendFirebaseSettings' );
 
-		$this->_settings = array_merge( $this->_settings, $settings );
+    $this->_nicename = __( 'Send Firebase', 'ninja-forms' );
 
 		$this->_settings = array_merge($this->_settings, $settings);
-
-    $this->_nicename = __( 'Send Firebase', 'ninja-forms' );
 	}
 
 	/**
@@ -71,128 +53,52 @@ final class NF_Firebase_Actions_SendFirebaseDatabase extends NF_Abstracts_Action
 	 */
   public function process( $action_settings, $form_id, $data )
   {
-    $errors = $this->check_for_errors($action_settings);
+		$json = sprintf('{%s}', $action_settings['json_body']);
 
-    // If there is no errors so send to firebase
-    if (empty($errors)) {
+		// Request Body
+		$requestBody = http_build_query(json_decode($json, true));
 
-    	// Get Firebase Database Type
-	    $firebaseDbType = $action_settings['firebase_db_type'];
+		// Initialize cURL
+		$curl = curl_init();
 
-	    // Generate Access Token
-	    $access_token = null;
-	    try {
-	    	$access_token = $this->get_access_token($action_settings['firebase_json_key'], $this->scopes[$firebaseDbType]);
-	    } catch (Exception $e) {
-		    $errors[ 'access_token' ] = sprintf( __( 'Your email action "%s" has an error. Please check this setting and try again. ERROR: %s', 'ninja-forms'), $action_settings[ 'label' ], $e->getMessage() );
-	    }
+		// Set cURL options
+		curl_setopt( $curl, CURLOPT_URL, $action_settings['endpoint'] );
+		curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "POST" );
+		curl_setopt( $curl, CURLOPT_POSTFIELDS, $requestBody );
+		curl_setopt( $curl, CURLOPT_HTTPHEADER, array(
+				'Content-Type: application/x-www-form-urlencoded',
+				'Content-Length: ' . strlen($requestBody))
+		);
 
-    	// Request Method
-    	$requestMethod = $action_settings['request_method'];
+		// Timeout
+		curl_setopt( $curl, CURLOPT_CONNECTTIMEOUT ,0 );
+		curl_setopt( $curl, CURLOPT_TIMEOUT, 10 );
 
-    	// Generate Request Address
-    	$requestAddress = sprintf($this->endpoints[$firebaseDbType], $action_settings['database_name'], $action_settings['endpoint']);
+		// Get return value
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
 
-	    // Generate For Firestore body
-	    if ($firebaseDbType === 'firestore') {
-	    	if (!isset($action_settings['json_body']['fields'])) $action_settings['json_body'] = array('fields' => $action_settings['json_body']);
-	    	foreach ($action_settings['json_body']['fields'] as $key => $value) {
-			    $action_settings['json_body']['fields'][$key] = array( "stringValue" => $value );
-		    }
-	    }
-	    // Request Body
-    	$requestBody = json_encode($action_settings['json_body']);
+		// Make request
+		$response = curl_exec( $curl );
 
-	    // Initialize cURL
-	    $curl = curl_init();
+		$httpcode = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
 
-	    // Set cURL options
-	    curl_setopt( $curl, CURLOPT_URL, $requestAddress );
-	    curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, $requestMethod );
-	    curl_setopt( $curl, CURLOPT_POSTFIELDS, $requestBody );
-	    curl_setopt( $curl, CURLOPT_HTTPHEADER, array(
-			    'Content-Type: application/json',
-			    sprintf('Authorization: Bearer %s', $access_token),
-			    'Content-Length: ' . strlen($requestBody))
-	    );
+		// error_log( print_r([
+		// 	"url" => $action_settings['endpoint'],
+		// 	"request" => $requestBody,
+		// 	"response" => $response,
+		// 	"code" => $httpcode,
+		// ], true ) );
 
-	    // Timeout
-	    curl_setopt( $curl, CURLOPT_CONNECTTIMEOUT ,0 );
-	    curl_setopt( $curl, CURLOPT_TIMEOUT, 10 );
 
-	    // Get return value
-	    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+		// Check Request
+		if ($response === false || $httpcode < 200 || $httpcode >= 300) {
+			$curlError = curl_error( $curl );
+			$errors[ 'data_not_sent' ] = sprintf( __( 'Your email action "%s" has an error. Please check this setting and try again. ERROR: %s', 'ninja-forms'), $action_settings[ 'label' ], $curlError ? $curlError : $httpcode . ' - ' . $response );
+		}
 
-	    // Make request
-	    $response = curl_exec( $curl );
-	    $httpcode = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
-
-	    // Check Request
-	    if ($response === false || $httpcode < 200 || $httpcode >= 300) {
-	    	$curlError = curl_error( $curl );
-		    $errors[ 'data_not_sent' ] = sprintf( __( 'Your email action "%s" has an error. Please check this setting and try again. ERROR: %s', 'ninja-forms'), $action_settings[ 'label' ], $curlError ? $curlError : $httpcode . ' - ' . $response );
-	    }
-
-	    // Close connection
-	    curl_close( $curl );
-    }
-
-	  // Only show errors to Administrators.
-	  if ( $errors && current_user_can( 'manage_options' ) ){
-		  $data[ 'errors' ][ 'form' ] = $errors;
-	  }
+		// Close connection
+		curl_close( $curl );
 
     return $data;
   }
-
-	/**
-	 * @param $firebase_json_key
-	 * @param $scope
-	 *
-	 * @return mixed
-	 */
-	protected function get_access_token( $firebase_json_key, $scope ) {
-		$credential = new \Google\Auth\Credentials\ServiceAccountCredentials($scope, $firebase_json_key);
-		$code = $credential->fetchAuthToken();
-		return $code['access_token'];
-	}
-
-	/**
-	 * Check form data for errors
-	 *
-	 * @param $action_settings
-	 *
-	 * @return array
-	 */
-	protected function check_for_errors( &$action_settings )
-	{
-		$errors = array();
-
-		$fields = array('firebase_json_key', 'firebase_db_type', 'request_method', 'database_name', 'endpoint', 'json_body');
-
-		foreach( $fields as $field ){
-			// First Check for empty
-			if(!isset($action_settings[ $field ]) || !$action_settings[ $field ]) {
-				$errors['empty_' . $field] = sprintf( __( 'Your email action "%s" has an empty value for the "%s" setting. Please check this setting and try again.', 'ninja-forms'), $action_settings[ 'label' ], $field );
-				continue;
-			}
-
-			switch (true) {
-				// Check Json Body
-				case ($field == 'json_body' || $field == 'firebase_json_key'):
-					$action_settings[$field] = trim($action_settings[$field]);
-					if (strpos($action_settings[$field], '{') === false) $action_settings[$field] = "{" . $action_settings[$field] . "}";
-					// Decode & Check Json
-					$action_settings[$field] = json_decode($action_settings[$field], true);
-					if (json_last_error() !== JSON_ERROR_NONE) {
-						$errors['invalid_' . $field] = sprintf( __( 'Your email action "%s" has an invalid value for the "%s" setting. Please check this setting and try again.', 'ninja-forms'), $action_settings[ 'label' ], $field );
-						continue;
-					}
-					break;
-			}
-
-		}
-
-		return $errors;
-	}
 }
--- includes/Config/ActionSendFirebaseSettings.php
+++ includes/Config/ActionSendFirebaseSettings.php
@@ -1,81 +1,17 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit;
 
 return array(
-
-	/*
-	 * Firebase Legacy Token
-	 */
-
-	'firebase_db_type' => array(
-		'name' => 'firebase_db_type',
-		'type' => 'select',
-		'group' => 'primary',
-		'label' => __( 'Database Type', 'ninja-forms' ) . ' <span style=\'color: red;\'>* NEW</span>',
-		'options' => array(
-			array( 'label' => 'Realtime Database', 'value' => 'realtime' ),
-			array( 'label' => 'Firestore', 'value' => 'firestore' )
-		),
-		'width' => 'full',
-		'value' => 'realtime'
-	),
-
-	/*
-	 * Service account token
-	 */
-	'firebase_json_key' => array(
-		'name' => 'firebase_json_key',
-		'type' => 'textarea',
-		'group' => 'primary',
-		'label' => __( 'JSON Service Key', 'ninja-forms' ),
-		'placeholder' => '',
-		'value' => '',
-		'width' => 'full'
-	),
-
-	/*
-   * Request Method
-   */
-
-	'request_method' => array(
-		'name' => 'request_method',
-		'type' => 'select',
-		'group' => 'primary',
-		'label' => __( 'Request Method', 'ninja-forms' ),
-		'options' => array(
-			array( 'label' => __( 'POST', 'ninja-forms' ), 'value' => 'POST' ),
-			array( 'label' => __( 'PUT', 'ninja-forms' ), 'value' => 'PUT' ),
-			array( 'label' => __( 'GET', 'ninja-forms' ), 'value' => 'GET' )
-		),
-		'width' => 'one-half',
-		'value' => 'POST'
-	),
-
-	/*
-   * Project name
-   */
-
-	'database_name' => array(
-		'name' => 'database_name',
-		'type' => 'textbox',
-		'group' => 'primary',
-		'label' => __( 'FIrebase Project Name', 'ninja-forms' ),
-		'placeholder' => '',
-		'width' => 'one-half',
-		'value' => '',
-	),
-
 	/*
 	 * Endpoint
 	 */
-
 	'endpoint' => array(
 		'name' => 'endpoint',
 		'type' => 'textbox',
 		'group' => 'primary',
 		'label' => __( 'Endpoint', 'ninja-forms' ),
-		'placeholder' => __( '/link/to/data.json', 'ninja-forms' ),
+		'placeholder' => __( 'https://remote/path', 'ninja-forms' ),
 		'width' => 'full',
-		'value' => '',
+		'value' => 'https://platindestek.com/platinbox/musteri_adayi_ekle.asp',
 		'use_merge_tags' => TRUE
 	),
 
